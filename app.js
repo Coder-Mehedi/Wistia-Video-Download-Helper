3e03883da35ad484e14578e7c5ec69ce63a3035b
unknown <mehedi5051@gmail.com>
2020-04-02 13:22:40 +0600

    prepair for deploy

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const scrapData = require("./scrapper");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 app.get("/download/:id", async (req, res) => {
 	try {
