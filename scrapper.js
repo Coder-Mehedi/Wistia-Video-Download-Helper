e9ce306c0957331392835527d913b35f717d9687
unknown <mehedi5051@gmail.com>
2020-04-02 11:11:52 +0600

    cleanup files

--- scrapper.js
+++ scrapper.js
@@ -1,11 +1,8 @@
-const fs = require("fs");
 const got = require("got");
 
-// const url = "http://fast.wistia.net/embed/iframe/e2vgflin81";
 const id = "e2vgflin81";
 
 const scrapData = async id => {
-	// const writeStream = fs.createWriteStream("data.json");
 	try {
 		const response = await got(`http://fast.wistia.net/embed/iframe/${id}`);
 
@@ -16,7 +13,6 @@ const scrapData = async id => {
 		let matched = html.match(regex);
 		if (matched !== null && matched.length > 0) {
 			matched = matched[0];
-			// matched = `[{ "url": "${matched}" }]`;
 			matched = `${matched}`;
 			// writeStream.write(matched);
 			return matched;
