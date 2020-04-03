0396118b60eacfc75eae3c136e753ec044e2803c
unknown <mehedi5051@gmail.com>
2020-04-03 22:08:34 +0600

    clean unnecessary code and screenshot updated

--- scrapper.js
+++ scrapper.js
@@ -1,14 +1,8 @@
 const got = require("got");
 
-// const id = "e2vgflin81";
-
-// regex for grabbing video
-// const rex = /wvideo=.{10}/;
-
 const scrapData = async id => {
 	try {
 		const response = await got(`http://fast.wistia.net/embed/iframe/${id}`);
-
 		const html = response.body;
 
 		// const regex = new RegExp(/http:\/\/embed.wistia.com\/deliveries\/.+.bin*/);
@@ -16,13 +10,11 @@ const scrapData = async id => {
 		let matched = html.match(regex);
 		if (matched !== null && matched.length > 0) {
 			matched = matched[0];
-			matched = `${matched}`;
-			// writeStream.write(matched);
 			return matched;
 		}
 	} catch (error) {
 		console.log(error);
 	}
 };
-// scrapData(id);
+
 module.exports = scrapData;
